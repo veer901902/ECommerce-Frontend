1ba89dcd82e7035d8ef8dd7affdbc74018da6b46
veer901902 <veer901902@gmail.com>
2024-02-03 15:09:31 +0530

    Added login and signup template with routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
 import Home from "./pages/Home";
 import "./App.css";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Login } from '../features/auth/components/Login'
+
+function LoginPage() {
+  return (
+    <Login></Login>
+  )
+}
+
+export default LoginPage
